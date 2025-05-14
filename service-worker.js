5fb0616a25d51782c44b74c84fd62356830ebd59
dlevanon <dlevanon2002@gmail.com>
2025-05-14 17:49:25 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+// sw.js - קובץ Service Worker
+const CACHE_NAME = 'shortcuts-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
